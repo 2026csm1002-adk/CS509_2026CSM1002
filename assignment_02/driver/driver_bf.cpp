c6aa2d30bb0a9b7d9707e06e463ac9ce53be1157
Aditya <2026csm1002@iitrpr.ac.in>
2026-08-09 16:57:16 +0530

    Written Driver code for Bellman-Ford Algorithm

---
+++ assignment_02/driver/driver_bf.cpp
@@ -0,0 +1,54 @@
+// Reads a directed, weighted adjacency-list graph, converts into CSR with the help of assignment 1
+#include "../../assignment_01/src/csr.h"
+#include "../src/bellman_ford.h"
+#include <chrono>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input_file>\n";
+        return 1;
+    }
+
+    string path = argv[1];
+    AdjacencyList adjList;
+
+    try {
+        adjList = readAdjacencyList(path, true); // Second parameter is weighted part
+    }
+    catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+
+    auto t1 = chrono::high_resolution_clock::now();
+    CSRGraph csr = adjacencyListToCSR(adjList);
+    int src = adjList.source;
+
+    BellmannFordResult result = bellmanFord(csr, src); 
+    auto t2 = chrono::high_resolution_clock::now();
+
+    double ms = chrono::duration<double, milli>(t2 - t1).count();
+
+    cout << "Algorithm: Bellman-Ford" << endl;
+    cout << "Source: " << src << endl;
+
+    if (result.negativeCycle) {
+        cout << "Negative cycle: true" << endl;
+    }
+    else {
+        cout << "Vertex Distance" << endl;
+        for (int v = 0; v < csr.V; ++v) {
+            cout << v << " ";
+            if (result.dist[v] >= BF_INF) cout << "INF";
+            else cout << result.dist[v];
+            cout << endl;
+        }
+        cout << "Negative cycle: none" << endl;
+    }
+
+    cout << "Execution time: " << ms << " ms" << endl;
+    return 0;
+}
